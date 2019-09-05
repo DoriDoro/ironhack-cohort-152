19873f49d247a7860613a4faa632b895fb053982
meta-one <meta-one@meta-one.local>
2019-09-05 11:12:06 +0200

    renamed crud-together folder

---
+++ module-2/week-2/day-4/0_crud_together_example/public/scripts/form_register.js
@@ -0,0 +1,16 @@
+const iconReveal = document.getElementById("reveal_password");
+const iconAvatar = document.getElementById("avatar");
+const inputFile = document.getElementById("file_input");
+const inputPassword = document.getElementById("password");
+
+iconAvatar.onclick = function displayFileBox() {
+  inputFile.click();
+};
+
+iconReveal.onclick = function reavealPassword() {
+  iconReveal.classList.toggle("fa-eye");
+  iconReveal.classList.toggle("fa-eye-slash");
+  inputPassword.type = iconReveal.classList.contains("fa-eye-slash")
+    ? "text"
+    : "password";
+};
